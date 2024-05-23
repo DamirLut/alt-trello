ad747d24ebad38bc4e1c6f22a740625df93be5bf
DamirLut <lutfrahmanovdamir3@gmail.com>
2024-05-23 23:17:56 +0500

    chore: :rotating_light: add no-misused-promises rule

--- eslint.config.js
+++ eslint.config.js
@@ -35,6 +35,12 @@ export default tsEslint.config(
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/require-await': 'off',
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        {
+          checksVoidReturn: false,
+        },
+      ],
       'simple-import-sort/imports': [
         'warn',
         {
